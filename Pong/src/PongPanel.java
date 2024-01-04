3ea54fc7ee0218e6daba6adf5c415063a6d915ae
MrSalmaan <salmaansham@ymail.com>
2024-01-05 11:57:03 +1300

    4th Commit

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,48 +1,60 @@
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class PongPanel extends JPanel implements ActionListener, KeyListener {
+public class PongPanel extends JPanel implements ActionListener {
 
-    private final static Color BACKGROUND_COLOR = Color.BLACK;
+    private final static Color BACKGROUND_COLOUR = Color.BLACK;
     private final static int TIMER_DELAY = 5;
 
+    private double ballX = 50;  // X-coordinate of the ball
+    private double ballY = 50;  // Y-coordinate of the ball
+    private double ballSpeedX = 2;  // Speed of the ball in the X direction
+    private double ballSpeedY = 2;  // Speed of the ball in the Y direction
+
     public PongPanel() {
-        setBackground(BACKGROUND_COLOR);
+        setBackground(BACKGROUND_COLOUR);
         Timer timer = new Timer(TIMER_DELAY, this);
         timer.start();
-        setFocusable(true);
-        addKeyListener(this);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        // Update game state in response to timer events
-        // ...
-        repaint(); // Repaint the panel
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Draw the ball
+        Ellipse2D.Double ball = new Ellipse2D.Double(ballX, ballY, 20, 20);
+        g2d.setColor(Color.WHITE);
+        g2d.fill(ball);
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
-        // Handle key presses
-        // ...
+    public void actionPerformed(ActionEvent event) {
+        update();
+        repaint();
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // Handle key releases
-        // ...
-    }
+    private void update() {
+        // Update ball position based on speed
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Handle key typing
-        // ...
+        // Reverse direction if ball hits the border
+        if (ballX < 0 || ballX > getWidth() - 20) {
+            ballSpeedX = -ballSpeedX;
+        }
+        if (ballY < 0 || ballY > getHeight() - 20) {
+            ballSpeedY = -ballSpeedY;
+        }
     }
 }
+
+
 
