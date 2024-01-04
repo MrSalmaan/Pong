8c03de41451cfd209669efae3a007229f3c90967
MrSalmaan <salmaansham@ymail.com>
2024-01-05 12:27:59 +1300

    7th commit
    This class now includes the setXPosition and setYPosition methods with
    the logic to keep the sprite within the bounds of the screen

--- Pong/src/Sprite.java
+++ Pong/src/Sprite.java
@@ -20,66 +20,64 @@ public class Sprite {
         this.color = color;
     }
 
-    // Getter and setter for xPosition
     public int getXPosition() {
         return xPosition;
     }
 
-    public void setXPosition(int xPosition) {
-        this.xPosition = xPosition;
+    public void setXPosition(int newX, int panelWidth) {
+        if (newX < 0) {
+            xPosition = 0;
+        } else if (newX + width > panelWidth) {
+            xPosition = panelWidth - width;
+        } else {
+            xPosition = newX;
+        }
     }
 
-    // Getter and setter for yPosition
     public int getYPosition() {
         return yPosition;
     }
 
-    public void setYPosition(int yPosition) {
-        this.yPosition = yPosition;
+    public void setYPosition(int newY, int panelHeight) {
+        if (newY < 0) {
+            yPosition = 0;
+        } else if (newY + height > panelHeight) {
+            yPosition = panelHeight - height;
+        } else {
+            yPosition = newY;
+        }
     }
 
-    // Getter and setter for xVelocity
     public int getXVelocity() {
         return xVelocity;
     }
 
-    public void setXVelocity(int xVelocity) {
-        this.xVelocity = xVelocity;
+    public void setXVelocity(int newXVelocity) {
+        xVelocity = newXVelocity;
     }
 
-    // Getter and setter for yVelocity
     public int getYVelocity() {
         return yVelocity;
     }
 
-    public void setYVelocity(int yVelocity) {
-        this.yVelocity = yVelocity;
+    public void setYVelocity(int newYVelocity) {
+        yVelocity = newYVelocity;
     }
 
-    // Getter and setter for width
     public int getWidth() {
         return width;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    // Getter and setter for height
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    // Getter and setter for color
     public Color getColor() {
         return color;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
+    public void setColor(Color newColor) {
+        color = newColor;
     }
 }
+
